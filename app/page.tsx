d8dbe6adbdf6e1c0f39e641eaf72f64082a44b15
humonnom <juepark42@gmail.com>
2024-07-19 03:23:50 +0900

    sign up form으로 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import SignupForm from "./components/SignupForm";
+
 export const metadata = {
   title: "App Router",
 };
 
 export default function Page() {
-  return <h1>App Router</h1>;
+  return <SignupForm/>
 }
